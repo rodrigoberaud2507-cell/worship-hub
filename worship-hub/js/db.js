6c7ce76aed62e1449f9b12e64720e17fd9796fca
rodrigoberaud2507-cell <rodrigoberaud2507@gmail.com>
2026-05-19 15:43:21 -0300

    Delete worship-hub directory

--- worship-hub/js/db.js
+++
@@ -1,8 +0,0 @@
-import Dexie from 'https://cdn.skypack.dev/dexie@3.2.4';
-
-export const db = new Dexie('WorshipHubDB');
-db.version(1).stores({
-  songs: 'id, title, _syncStatus',
-  setlists: 'id, name, _syncStatus',
-  pendingChanges: '++id, tableName, recordId, timestamp'
-});
