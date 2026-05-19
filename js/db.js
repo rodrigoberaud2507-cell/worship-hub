a7ada3a52f3c9d38646f55e84ac6ec3d9442beae
rodrigoberaud2507-cell <rodrigoberaud2507@gmail.com>
2026-05-19 17:14:55 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,19 +1,9 @@
 import Dexie from 'https://cdn.skypack.dev/dexie@3.2.4';
 
 export const db = new Dexie('WorshipHubDB');
-
 db.version(1).stores({
   songs: 'id, title, artist, key, bpm, _syncStatus',
   setlists: 'id, name, date, _syncStatus',
   events: 'id, name, date, _syncStatus',
   pendingChanges: '++id, tableName, recordId, operation, timestamp'
 });
-
-// Helpers para estado offline
-export async function getOfflineSongs() {
-  return await db.songs.toArray();
-}
-
-export async function getPendingCount() {
-  return await db.pendingChanges.count();
-}
